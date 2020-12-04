d116f80ac699398913b59e5951ce079bc5775bec
i92exfej <i92exfej@uco.es>
2020-12-04 22:54:17 +0100

    Commit para introducir las clases obtenerDatosParque() y setDatosParque() ademas de probar como funcionaria un commit

--- GM1_equipo4/Clases.cc
+++ GM1_equipo4/Clases.cc
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+struct Horario{
+	string dias;
+	string horas;
+};
+
+struct Premio{
+	string nombre;
+	string fecha;
+	string descripcion;
+};
+
+struct Reconocimiento{
+	string nombre;
+	string fecha;
+	string descripcion;
+};
+
 class Usuario{
 	private:
 		string password_;
@@ -51,10 +68,95 @@ class Administrativo:public Usuario{
 
 	public:
 		Administrartivo(string dni, string password);
-		string getDNI() override;
+		string getDNI() override{
+			return dni_;
+		}
 
-		void obtenerDatosParque();
-		void setDatosParque();
+		void obtenerDatosParque(){
+			Parque P;
+			P=parque; //objeto de la clase parque declarado como global en el programa main
+			cout<<"Nombre del parque: "<<P.Parque::mostrarNombre()<<endl;
+			cout<<"Ubicacion del parque: "<<P.Parque::mostrarUbicacion<<endl;
+			cout<<"Superficie del parque "<<P.Parque::mostrarSuperficie<<" hectareas"<<endl;
+			cout<<"Zonas contenidas en el parque"<<P.Parque::mostrarZona<<endl;
+			cout<<"Fecha de declaracion de parque natural: "<<P.Parque::mostrarFechaDeclaracion<<endl;
+			cout<<"El parque ha obtenido los premios: "<<P.Parque::mostrarPremios<<endl;
+			cout<<"El parque tiene los siguientes reconocimientos: "<<P.Parque::mostarReconocimientos<<endl;
+			cout<<"El horario del parque es: "<<P.Parque::mostrarHorario<<endl;
+			cout<<"Las rutas disponibles en el parque son: "<<P.Parque::mostrarRutas<<endl;
+
+		};
+		void setDatosParque(){
+			Premio aux1; //Auxiliar del tipo struct Premio
+			Reconocimiento aux2; //Auxiliar del tipo struct Reconocimiento
+			Horario aux3; // Auxiliar del tipo struct Horario
+			Parque P;
+			P=parque; //objeto de la clase parque declarado como global en el programa main
+			int opcion;
+
+			cout<<"Elija opcion de modificacion:"<<endl;
+			cout<<"1. Modificar horario"<<endl;
+			cout<<"2. Borrar Premio"<<endl;
+			cout<<"3. Añadir nuevo Premio"<<endl;
+			cout<<"4. Borrar reconocimiento"<<endl;
+			cout<<"5. Añadir nuevo reconocimiento"<<endl;
+			cin>>opcion;
+			switch(opcion){
+			case 1:
+				cout<<"Introduzca el nuevo horario: "<<endl;
+				cout<<"Dias de apertura: "; cin>>aux3.dias;
+				cout<<"\nHoras de trabajo :"; cin>>aux3.horas;
+			if(!(P.Parque::cambiarhorario(aux3))){
+				cout<<"Error cambiando el horario"<<endl;
+			}
+				break;
+			case 2:
+				if(P.Paque::borrarPremio()){
+					cout<<"Premio borrado con exito"<<endl;
+				}
+				else{
+					cout<<"Error al borrar premio"<<endl;
+				}
+				break;
+			case 3:
+				cout<<"Introduczca nuevo premio: "<<endl;
+				cout<<"Introduzca nombre: "; cin>>aux1.nombre;
+				cout<<"\nIntroduzca fecha: "; cin>>aux1.fecha;
+				cout<<"\nIntroduzca descripcion: "; cin>>aux1.descripcion;
+				if(P.Parque::añadirPremio(aux1)){
+					cout<<"\nPremio introducido con exito"<<endl;
+				}
+				else{
+					cout<<"Error al introducir premio"<<endl;
+				}
+				break;
+			case 4:
+				if(P.Parque::borrarReconocimiento()){
+					cout<<"Reconocimiento borrado con exito;"<<endl;
+
+				}
+				else{
+					cout<<"Borrado de reconocimiento fallido"<<endl;
+				}
+				break;
+			case 5:
+				cout<<"Introduczca nuevo reconocimiento: "<<endl;
+				cout<<"Introduzca nombre: "; cin>>aux2.nombre;
+				cout<<"\nIntroduzca fecha: "; cin>>aux2.fecha;
+				cout<<"\nIntroduzca descripcion: "; cin>>aux2.descripcion;
+				if(P.Parque::añadirReconocimiento(aux2)){
+					cout<<"\nReconocimiento introducido con exito"<<endl;
+				}
+				else{
+					cout<<"Error al introducir reconocimiento"<<endl;
+				}
+				break;
+			default:
+				cout<<"El numero introducido no se corresponde con ninguna opcion"<<endl;
+			}
+		}
+
+
 
 		void obtenerDatosRutas();
 		void changeRuta();
